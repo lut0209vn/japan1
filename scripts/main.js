75ae8dff893a429b10ae2b5c043cba9e596ff2b5
lut0209vn <lut0209vn@gmail.com>
2018-01-02 00:20:06 +0700

    a

--- scripts/main.js
+++ scripts/main.js
@@ -32,12 +32,49 @@ var myInterval=setInterval(function(){
       $(".backstretch1 img:first-child").addClass("active");
     }
 },4000);
-// $(document).ready(function(){
-  $(window).scroll(function(){
-  	if ($(this).scrollTop() > 10) {
-      $(".page-header").css("position","fixed");
-  	} else {
-      $(".page-header").css("position","relative");
-  	}
-  });
-// }
+
+$(window).scroll(function(){
+	if ($(this).scrollTop() > 10) {
+    $(".page-header").css("position","fixed");
+	} else {
+    $(".page-header").css("position","relative");
+	}
+});
+//wow
+new WOW().init();
+//scroll
+$(document).ready(function () {
+            //Smooth scrolling when click to nav
+            $('.nav-main li a').click(function (e) {
+                e.preventDefault();
+                var curLink = $(this);
+                var scrollPoint = $(curLink.attr('href')).position().top;
+                $('body,html').animate({
+                    scrollTop: scrollPoint
+                }, 800);
+            })
+            $(window).scroll(function () {
+                onScrollHandle();
+            });
+            // function onScrollHandle() {
+            //     //Navbar shrink when scroll down
+            //     $(".navbar-default").toggleClass("navbar-shrink", $(this).scrollTop() > 50);
+            //     //Get current scroll position
+            //     var currentScrollPos = $(document).scrollTop();
+            //     //Iterate through all node
+            //     $('.nav-main li a').each(function () {
+            //         var curLink = $(this);
+            //         var refElem = $(curLink.attr('href'));
+            //         //Compare the value of current position and the every section position in each scroll
+            //         if (refElem.position().top <= currentScrollPos && refElem.position().top + refElem.height() > currentScrollPos) {
+            //             //Remove class active in all nav
+            //             $('.nav-main li ').removeClass("active");
+            //             //Add class active
+            //             curLink.parent().addClass("active");
+            //         }
+            //         else {
+            //             curLink.parent().removeClass("active");
+            //         }
+            //     });
+            // }
+        });
